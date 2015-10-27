4d1494b4d90d29e55189561a30ec0f1fbec2c506
Hexrus <mahoe7@hawaii.edu>
2015-10-27 01:52:46 -1000

    ICS314

--- collections/polls.js
+++ collections/polls.js
@@ -3,3 +3,13 @@
  */
 
 Polls = new Mongo.Collection('polls');
+
+if(Meteor.isServer) {
+  Meteor.publish("polls", function() {
+    return Polls.find();
+  });
+}
+
+if(Meteor.isClient) {
+  Meteor.subscribe("polls");
+}
